19b774e8dabc7c81406c96962fdc51b4daa51066
Ivan Zimin <istinx@gmail.com>
2015-09-08 21:16:04 +0300

    Наводим порядок

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import messager
+from messager import messager_test
+
+#ansv=messager.messager_test('ЖОПА'):
+
+#if ansv:
+
+ #   print('OK')
+answ = messager_test('Привет')
+
+if answ:
+
+    print("OK")
+
+print answ
